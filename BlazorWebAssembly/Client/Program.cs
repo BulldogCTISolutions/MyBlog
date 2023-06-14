e5aeb1f95d28d01a6a321eb7ec1ba212f2cb6f8b
Jay McCormick <jay.ctiguru@gmail.com>
2023-06-14 10:34:10 -0700

    Chapter 8 - After adjusting Auth0

--- BlazorWebAssembly/Client/Program.cs
+++ BlazorWebAssembly/Client/Program.cs
@@ -1,9 +1,16 @@
-using System.Net;
+using System.Reflection;
+
+using Components.RazorComponents;
+
+using Data.Models.Interfaces;
 
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace BlazorWebAssembly.Client;
+
 public static class Program
 {
     public static async Task Main( string[] args )
@@ -14,10 +21,35 @@ public static class Program
 
         _ = builder.Services.AddScoped( sp => new HttpClient
         {
-            BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ),
-            DefaultRequestVersion = HttpVersion.Version30,
-            DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
+            BaseAddress = new Uri( builder.HostEnvironment.BaseAddress )
         } );
+
+        // Add Configuration stack
+        // Add keys and secrets to builder configuration
+        _ = builder.Configuration
+                   .SetBasePath( AppDomain.CurrentDomain.BaseDirectory )
+                   //.AddJsonFile( "appSettings.json" )
+                   // Passing “false” as the second variable for UserSecrets
+                   // That’s because in .NET 6, User Secrets were made “required” by default
+                   // and by passing true, we make them optional. 
+                   .AddUserSecrets( Assembly.GetExecutingAssembly().GetCustomAttribute<UserSecretsIdAttribute>()!.UserSecretsId, false )
+                   .Build();
+
+        _ = builder.Services.AddHttpClient( "Public",
+                                    client => client.BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) );
+        _ = builder.Services.AddHttpClient( "Authenticated",
+                                    client => client.BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) )
+                            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+        _ = builder.Services.AddOidcAuthentication( options =>
+        {
+            builder.Configuration.Bind( "Auth0", options.ProviderOptions );
+            options.ProviderOptions.ResponseType = "code";
+            //options.ProviderOptions.AdditionalProviderParameters.Add( "audience", builder.Configuration["Auth0:Audience"] );
+        } );
+
+        _ = builder.Services.AddTransient<IBlogApi, BlogApiWebClient>();
+        _ = builder.Services.AddTransient<ILoginStatus, LoginStatusWasm>();
 
         await builder.Build().RunAsync().ConfigureAwait( false );
     }
